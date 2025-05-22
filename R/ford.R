635824d61aea8e119c099b72df51c7add8771bc0
Pouya Roudaki <roudaki.pouya@gmail.com>
2025-05-22 09:44:19 +0100

    a small change on ford example

--- R/ford.R
+++ R/ford.R
@@ -171,10 +171,10 @@ ford_main <- function(Y, X, dist.type.X = "continuous", num_features = NULL, sto
 #' @examples
 #' # Example 1
 #' n = 500
-#' p = 100
+#' p = 10
 #' x <- matrix(rnorm(n * p), nrow = n)
 #' colnames(x) = paste0(rep("x", p), seq(1, p))
-#' y <- x[, 1] * x[, 10] + x[, 20]^2
+#' y <- x[, 1] * x[, 8] + x[, 10]^2
 #' # with num_features equal to 3 and stop equal to FALSE, ford will give a list of
 #' # three selected features
 #' result1 = ford(y, x, num_features = 3, stop = FALSE, numCores = 1)
